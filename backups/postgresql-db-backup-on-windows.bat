923044b17cf34ca40219d71d86968d9ab9449c50
Totally Tot <fcukthepop@gmail.com>
2018-11-09 12:53:22 +0300

    Update postgresql-db-backup-on-windows.bat

--- backups/postgresql-db-backup-on-windows.bat
+++ backups/postgresql-db-backup-on-windows.bat
@@ -15,6 +15,9 @@ set host=127.0.0.1
 set pg_dump="D:\Program Files\PostgreSQL\9.6\bin\pg_dump.exe"
 set PGPASSWORD=<PASSWORD FOR POSTGRES USER>
 
+if not exist %BACKUP_DIR%   mkdir %BACKUP_DIR%
+
 echo on
 %pg_dump% -h %host% -p 5432 -U postgres -F c -b -v -f %BACKUP_DIR%\%BACKUP_FILE% %db%
 %zip% a -tgzip %BACKUP_DIR%\%BACKUP_FILE%.gz %BACKUP_DIR%\%BACKUP_FILE%
+del %BACKUP_DIR%\%BACKUP_FILE%
