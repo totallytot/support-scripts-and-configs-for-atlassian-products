b5fe81954e6a21b7b7c73eb73fdc6eb3fad3ba60
Totally Tot <fcukthepop@gmail.com>
2019-01-28 12:48:27 +0300

    Create pfx-to-cert-key.cmd

---
+++ ssl/pfx-to-cert-key.cmd
@@ -0,0 +1,8 @@
+REM export the private key:
+"c:\Program Files\OpenSSL-Win64\bin\openssl.exe" pkcs12 -in jira.pfx -nocerts -out key.pem -nodes
+
+REM export the certificate:
+"c:\Program Files\OpenSSL-Win64\bin\openssl.exe" pkcs12 -in jira.pfx -nokeys -out cert.pem
+
+REM remove the passphrase from the private key:
+"c:\Program Files\OpenSSL-Win64\bin\openssl.exe" rsa -in key.pem -out private.key
