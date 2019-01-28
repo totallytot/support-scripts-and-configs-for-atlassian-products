74bf5c1818d0ce96dd1b18b6c4f7d6b1920c3577
Totally Tot <fcukthepop@gmail.com>
2019-01-28 21:56:51 +0300

    Update pfx-to-cert-key.cmd

--- ssl/pfx-to-cert-key.cmd
+++ ssl/pfx-to-cert-key.cmd
@@ -8,4 +8,4 @@ REM remove the passphrase from the private key:
 "c:\Program Files\OpenSSL-Win64\bin\openssl.exe" rsa -in key.pem -out private.key
 
 REM convert cert to human-readable fromat:
-"c:\Program Files\OpenSSL-Win64\bin\openssl.exe"  base64 -in intermediate.crt -out inter.crt
+"c:\Program Files\OpenSSL-Win64\bin\openssl.exe" base64 -in intermediate.crt -out inter.crt
