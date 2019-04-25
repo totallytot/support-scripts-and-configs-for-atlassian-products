baa79088e93cf2dbf476f82e4365330bd46db622
Totally Tot <fcukthepop@gmail.com>
2019-04-25 14:29:19 +0300

    Update postgre-db-confluence-home-install-backup.sh

--- backups/postgre-db-confluence-home-install-backup.sh
+++ backups/postgre-db-confluence-home-install-backup.sh
@@ -7,15 +7,15 @@ DIR_C_HOME="/var/atlassian/application-data/confluence"
 ########
 mkdir $BACK_DIR_C/db
 pg_dump --host 127.0.0.1 --port 5432 --username postgres --blobs --quote-all-identifiers --verbose --file /$BACK_DIR_C/db/$D-confluence.psql confluence
-tar -czvf $BACK_DIR_C/$D-confluence.psql.tar.gz $BACK_DIR_C/db/$D-confluence.psql
-tar -czvf $BACK_DIR_C/$D-confluence-home.tar.gz $DIR_C_HOME \
+tar -czpvf $BACK_DIR_C/$D-confluence.psql.tar.gz $BACK_DIR_C/db/$D-confluence.psql
+tar -czpvf $BACK_DIR_C/$D-confluence-home.tar.gz $DIR_C_HOME \
 --exclude=/var/atlassian/application-data/confluence/backups \
 --exclude=/var/atlassian/application-data/confluence/restore \
 --exclude=/var/atlassian/application-data/confluence/*plugins* \
 --exclude=/var/atlassian/application-data/confluence/*logs \
 --exclude=/var/atlassian/application-data/confluence/recovery \
 --exclude=/var/atlassian/application-data/confluence/temp
-tar -czvf $BACK_DIR_C/$D-confluence-opt.tar.gz $DIR_C \
+tar -czpvf $BACK_DIR_C/$D-confluence-opt.tar.gz $DIR_C \
 --exclude=/opt/atlassian/confluence/logs \
 --exclude=/opt/atlassian/confluence/temp \
 --exclude=/opt/atlassian/confluence/work
